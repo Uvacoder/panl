b10ca1ef1ddd05e9d596df44ecf9ff87fb61b91e
Tu Trinh <tu.trinh@heart.org>
2021-10-11 17:22:22 -0500

    testing

--- src/App.vue
+++ src/App.vue
@@ -1603,7 +1603,7 @@ export default {
 // Check out https://github.com/vuejs/rfcs/blob/master/active-rfcs/0040-script-setup.md
 </script>
 <!-- ANCHOR: STYLE -->
-<style global lang="scss">
+<style lang="scss">
 @import '@/assets/sass/test.scss';
 
 .container {
